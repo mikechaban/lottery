31b0518dc84bda7c5d7a7d08ce91669a5fabd6c2
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-05-15 20:36:42 -0400

    changed bgc

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head />
-            <body className="bg-fuchsia-300">
+            <body className="bg-slate-200">
                 <Main />
                 <NextScript />
             </body>
