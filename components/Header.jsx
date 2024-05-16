2a3866f1ab11a2ef69609e7dda955eac5841eb14
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-05-15 20:55:34 -0400

    changed border color

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import { ConnectButton } from "web3uikit"
 
 export default function Header() {
     return (
-        <div className="p-5 border-b-2 flex flex-row">
+        <div className="p-5 border-b-2 border-sky-800 flex flex-row">
             <h1 className="py-4 px-4 font-bold text-3xl text-sky-800">Decentralized Lottery</h1>
             <div className="ml-auto py-2 px-4">
                 <ConnectButton /* moralisAuth={false} -> shows it that we're not trying to connect to a server*/
