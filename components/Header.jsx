c8199ebfd8c96807a16f5e49a8b65b8de50c8708
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-05-19 01:47:49 -0400

    added a warning message

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,6 +4,10 @@ export default function Header() {
     return (
         <div className="p-5 border-b-2 border-sky-800 flex flex-row">
             <h1 className="py-4 px-4 font-bold text-3xl text-sky-800">Decentralized Lottery</h1>
+            <div>
+                This is a test project that only uses the Ethereum Sepolia testnet. The lottery is
+                not suited for use with actual funds.
+            </div>
             <div className="ml-auto py-2 px-4">
                 <ConnectButton /* moralisAuth={false} -> shows it that we're not trying to connect to a server*/
                 />
